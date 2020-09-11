2d571493eb8e1648e839056706997d3913cd4211
Vansh <vansharora2929@gmail.com>
2020-09-12 00:41:19 +0530

    added a temporary main method

--- DataStructures/linked_list.c
+++ DataStructures/linked_list.c
@@ -163,6 +163,26 @@ void remove_last_node()
 // Driver Function
 int main()
 {
-
-    return 0;
+/*int choice;
+while(1){
+printf("Press 1 to Push\n");
+printf("Press 2 to Pop\n");
+printf("Press 3 to Delete\n");
+printf("Press 4 to Exit\n");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:
+//Push();
+break;
+case 2:
+//Pop();
+break;
+case 3:
+//Delete();
+break;
+case 4:
+return 0;
+}    
+}*/
 }
